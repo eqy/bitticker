d23cc91dcab0a8267044845fc21d16335c483aef
ketsol <Loser777@gmail.com>
2013-04-13 13:19:35 -0700

    parsing updates, arduino code prototyped with incrementin

--- bitticker.py
+++ bitticker.py
@@ -1,7 +1,18 @@
+import serial
 import urllib.request
 from bs4 import BeautifulSoup
+import time
 
-page = urllib.request.urlopen('https://mtgox.com')
-soup = BeautifulSoup(page);
-result = soup.find(id='lastPrice').span.contents[0]
-print(result[1:])
+ser = serial.Serial('/dev/ttyACM0', 9600)
+        
+while True:
+        page = urllib.request.urlopen('https://mtgox.com')
+        soup = BeautifulSoup(page);
+        result = soup.find(id='lastPrice').span.contents[0]
+        print(result[1:])
+
+        #Write to Arduino
+        ser.write(result[1:].encode('utf-8'))
+        time.sleep(30)
+        
+        
