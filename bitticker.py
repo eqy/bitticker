8cf671ce58f9750feca9a237f0c04e56d3506816
ketsol <Loser777@gmail.com>
2013-04-13 13:33:27 -0700

    parsing updates, arduino code prototyped with incrementin

--- bitticker.py
+++ bitticker.py
@@ -6,7 +6,12 @@ import time
 ser = serial.Serial('/dev/ttyACM0', 9600)
         
 while True:
-        page = urllib.request.urlopen('https://mtgox.com')
+        try: 
+          page = urllib.request.urlopen('https://mtgox.com')
+          break
+        except IOError as e:
+          if e.args[1].args[0].errno != errno.ECONNRESET:
+             raise
         soup = BeautifulSoup(page);
         result = soup.find(id='lastPrice').span.contents[0]
         print(result[1:])
